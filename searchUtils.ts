e8b02796649c02c50debef9fd478347018117bdb
Alistair Vu <alistairvu@gmail.com>
2021-01-20 15:29:30 +0700

    Add album search functionality

--- searchUtils.ts
+++ searchUtils.ts
@@ -34,4 +34,37 @@ const getSongs = async (query: string) => {
   }
 }
 
+const getAlbums = async (query: string) => {
+  try {
+    const token = await getToken()
+    const res = await fetch(
+      `https://api.spotify.com/v1/search?q=${query}&type=track`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    const json = await res.json()
+    const rawData = await json.albums.items
+
+    const albumData: Array<albumData> = rawData.map((item: any) => {
+      const { artists, images, name, release_date, id } = item
+      return {
+        artist: artists.map((x: { name: string }) => x.name).join(", "),
+        image: images[0].url,
+        albumName: name,
+        releaseDate: release_date,
+        albumId: id,
+      }
+    })
+    return albumData
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export { getSongs }
