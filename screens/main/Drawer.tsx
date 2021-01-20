f38f0e0055909d6be9c28595727e67c3df970ae0
Alistair Vu <alistairvu@gmail.com>
2021-01-20 16:34:07 +0700

    Add drawer tint colour

--- screens/main/Drawer.tsx
+++ screens/main/Drawer.tsx
@@ -8,7 +8,11 @@ const Drawer = createDrawerNavigator()
 
 const DrawerScreen: React.FC = () => {
   return (
-    <Drawer.Navigator drawerType="back" minSwipeDistance={500}>
+    <Drawer.Navigator
+      drawerType="back"
+      minSwipeDistance={500}
+      drawerContentOptions={{ activeTintColor: "#00ce3b" }}
+    >
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="Top Songs" component={Charts} />
       <Drawer.Screen name="Library" component={Library} />
